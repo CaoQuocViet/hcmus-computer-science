20a53273bc31b20ae58a3eb88f6c0e993a45e283
CaoQuocViet <vitthongthai02@gmail.com>
2025-03-25 10:54:14 +0700

    feat(services): implement ILastPageService and LastPageService for managing last visited page state - Create ILastPageService interface with methods for getting, saving, and clearing the last page - Implement LastPageService that utilizes ILocalSettingsService for persistent storage of last page information

---
+++ CSC13122-LT-UNG-DUNG-QUAN-LY-2/StormPC/StormPC/Services/LastPageService.cs
@@ -0,0 +1,38 @@
+using StormPC.Contracts.Services;
+using StormPC.Core.Helpers;
+using StormPC.Helpers;
+
+namespace StormPC.Services;
+
+public class LastPageService : ILastPageService
+{
+    private const string SettingsKey = "LastPageType";
+    private readonly ILocalSettingsService _localSettingsService;
+
+    public LastPageService(ILocalSettingsService localSettingsService)
+    {
+        _localSettingsService = localSettingsService;
+    }
+
+    public async Task<string?> GetLastPageAsync()
+    {
+        return await _localSettingsService.ReadSettingAsync<string>(SettingsKey);
+    }
+
+    public async Task SaveLastPageAsync(string pageType)
+    {
+        await _localSettingsService.SaveSettingAsync<string>(SettingsKey, pageType);
+    }
+
+    public async Task ClearLastPageAsync()
+    {
+        if (RuntimeHelper.IsMSIX)
+        {
+            Windows.Storage.ApplicationData.Current.LocalSettings.Values.Remove(SettingsKey);
+        }
+        else
+        {
+            await _localSettingsService.SaveSettingAsync<string>(SettingsKey, string.Empty);
+        }
+    }
+} 
