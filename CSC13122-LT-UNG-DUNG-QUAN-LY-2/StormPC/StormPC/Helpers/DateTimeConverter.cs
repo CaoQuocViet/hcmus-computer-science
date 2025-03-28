c1ed5678f133a997c5b167f43b5b5d53c49be9de
CaoQuocViet <vitthongthai02@gmail.com>
2025-03-28 20:13:23 +0700

    feat(helpers): add DateTimeConverter for converting between DateTime and DateTimeOffset

---
+++ CSC13122-LT-UNG-DUNG-QUAN-LY-2/StormPC/StormPC/Helpers/DateTimeConverter.cs
@@ -0,0 +1,25 @@
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace StormPC.Helpers;
+
+public class DateTimeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is DateTime dateTime)
+        {
+            return new DateTimeOffset(dateTime);
+        }
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        if (value is DateTimeOffset dateTimeOffset)
+        {
+            return dateTimeOffset.DateTime;
+        }
+        return DateTime.Now;
+    }
+} 
