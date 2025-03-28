5278a18cc27a73d7499dab0bbe49862eac8f9a71
CaoQuocViet <vitthongthai02@gmail.com>
2025-03-28 21:11:57 +0700

    feat(database): update payment method creation timestamps to fixed dates in seed data

--- CSC13122-LT-UNG-DUNG-QUAN-LY-2/DataBase/seeders/20250320070327-payment-methods.js
+++ CSC13122-LT-UNG-DUNG-QUAN-LY-2/DataBase/seeders/20250320070327-payment-methods.js
@@ -3,55 +3,54 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const now = new Date();
     await queryInterface.bulkInsert('PaymentMethods', [
       {
         PaymentMethodID: 1,
         MethodName: 'Thẻ ATM nội địa',
-        createdAt: now,
-        updatedAt: now
+        createdAt: new Date('2025-01-08T11:00:00Z'),
+        updatedAt: new Date('2025-01-08T11:00:00Z')
       },
       {
         PaymentMethodID: 2,
         MethodName: 'Ví MoMo',
-        createdAt: now,
-        updatedAt: now
+        createdAt: new Date('2025-01-08T11:05:00Z'),
+        updatedAt: new Date('2025-01-08T11:05:00Z')
       },
       {
         PaymentMethodID: 3,
         MethodName: 'VNPay',
-        createdAt: now,
-        updatedAt: now
+        createdAt: new Date('2025-01-08T11:10:00Z'),
+        updatedAt: new Date('2025-01-08T11:10:00Z')
       },
       {
         PaymentMethodID: 4,
         MethodName: 'ZaloPay',
-        createdAt: now,
-        updatedAt: now
+        createdAt: new Date('2025-01-08T11:15:00Z'),
+        updatedAt: new Date('2025-01-08T11:15:00Z')
       },
       {
         PaymentMethodID: 5,
         MethodName: 'Thẻ tín dụng/ghi nợ',
-        createdAt: now,
-        updatedAt: now
+        createdAt: new Date('2025-01-08T11:20:00Z'),
+        updatedAt: new Date('2025-01-08T11:20:00Z')
       },
       {
         PaymentMethodID: 6,
         MethodName: 'Chuyển khoản ngân hàng',
-        createdAt: now,
-        updatedAt: now
+        createdAt: new Date('2025-01-08T11:25:00Z'),
+        updatedAt: new Date('2025-01-08T11:25:00Z')
       },
       {
         PaymentMethodID: 7,
         MethodName: 'Thanh toán khi nhận hàng (COD)',
-        createdAt: now,
-        updatedAt: now
+        createdAt: new Date('2025-01-08T11:30:00Z'),
+        updatedAt: new Date('2025-01-08T11:30:00Z')
       },
       {
         PaymentMethodID: 8,
         MethodName: 'ShopeePay',
-        createdAt: now,
-        updatedAt: now
+        createdAt: new Date('2025-01-08T11:35:00Z'),
+        updatedAt: new Date('2025-01-08T11:35:00Z')
       }
     ], {});
   },
