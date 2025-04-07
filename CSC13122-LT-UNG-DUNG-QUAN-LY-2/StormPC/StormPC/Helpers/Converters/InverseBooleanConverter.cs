70a4608b3e19e2bb6a48bddaf097f7f6d7f05a83
CaoQuocViet <vitthongthai02@gmail.com>
2025-04-07 19:29:09 +0700

    feat(converters): add InverseBooleanConverter for boolean value inversion
    - Implemented InverseBooleanConverter to facilitate the conversion of boolean values to their inverse for use in data binding scenarios.
    - The converter supports both forward and reverse conversion, enhancing flexibility in UI data binding.

---
+++ CSC13122-LT-UNG-DUNG-QUAN-LY-2/StormPC/StormPC/Helpers/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,24 @@
+using Microsoft.UI.Xaml.Data;
+
+namespace StormPC.Helpers;
+
+public class InverseBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is bool boolValue)
+        {
+            return !boolValue;
+        }
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        if (value is bool boolValue)
+        {
+            return !boolValue;
+        }
+        return value;
+    }
+} 
